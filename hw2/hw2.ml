637255ed23eb347e989aef68e3599f2142481bd5
Eric Altenburg <ealtenbu@stevens.edu>
2020-02-10 11:45:56 -0500

    Added initial vars and functions to complete

---
+++ hw2/hw2.ml
@@ -0,0 +1,35 @@
+(* 	Name: 			Eric Altenburg
+	Description: 	Implementation of DTree
+	Pledge: 		I pledge my honor that I have abided by the Stevens Honor System.
+	Date: 			10 February 2020 
+*)
+
+let tLeft = failwith "Implement"
+
+let tRight = failwith "Implement"
+
+let dTree = tLeft || tRight
+
+let dTree_height (t:dTree) : int = 
+	failwith "Implement"
+
+let dTree_size (t:dTree) : int =
+	failwith "Implement"
+
+let dTree_paths (t:dTree) : int list list = 
+	failwith "Implement"
+
+let dTree_is_perfect (t:dTree) : bool = 
+	failwith "Implement"
+
+let dTree_map f g (t:dTree) : dTree = 
+	failwith "Implement"
+
+let list_to_tree (l: char list) : dTree = 
+	failwith "Implement"
+
+let replace_leaf_at (t:dTree) f : dTree = 
+	failwith "Implement"
+
+let bf_to_dTree = 
+	failwith "Implement"
