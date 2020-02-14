e43283b3a9aac253007cd88a472c4c0352aa02bc
Eric Altenburg <ealtenbu@stevens.edu>
2020-02-13 20:55:40 -0500

    Did a few more problems for hw2

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -6,29 +6,62 @@
 
 type ('a, 'b) dTree = Leaf of 'a | Node of 'b * ('a,'b) dTree * ('a,'b) dTree
 
-let tLeft = failwith "Implement"
+let tBig = Node ('a',
+				Node('r',
+					Node('c', Leaf 1, Leaf 2),
+					Node('d', Leaf 3, Leaf 4)),
+				Node('j',
+					Node('u',
+						Node('y', Leaf 5, Leaf 6),
+						Leaf 8),
+					Leaf 4))
 
-let tRight = failwith "Implement"
+let tLeft = Node('w',
+				Node('x', Leaf 2, Leaf 5),
+				Leaf 8)
 
-let dTree_height (t:dTree) : int = 
+let tRight = Node('w',
+				Node('x', Leaf 2, Leaf 5),
+				Node('y', Leaf 7, Leaf 5))
+
+let rec dTree_height_h t i : int =
+	match t with
+	| Leaf t -> i
+	| Node (d, lt, rt) -> max (dTree_height_h lt (i+1)) (dTree_height_h rt (i+1))
+
+let dTree_height t : int = 
+	dTree_height_h t 0
+
+let rec dTree_size_h t i: int = 
+	match t with
+	| Leaf t -> 1
+	| Node (d, lt, rt) -> 1 + (dTree_size_h lt i) + (dTree_size_h rt i)
+
+let dTree_size t : int =
+	dTree_size_h t 1
+
+let rec dTree_paths_h (t: ('a, 'b) dTree) (way:int) =
+	match t with
+	| Leaf t -> []
+	| Node (d, lt, rt) -> List.cons (way :: (dTree_paths_h lt 0)) (way :: (dTree_paths_h rt 1))
+
+let dTree_paths (t: ('a, 'b) dTree) = 
+	dTree_paths_h t 0 :: dTree_paths_h t 1 :: []
+	
+(* let rec dTree_is_perfect_h t d : bool =
+
+
+
+let dTree_is_perfect t : bool = 
+	dTree_is_perfect_h t 0 *)
+
+let dTree_map f g t = 
 	failwith "Implement"
 
-let dTree_size (t:dTree) : int =
+let list_to_tree (l: char list) = 
 	failwith "Implement"
 
-let dTree_paths (t:dTree) : int list list = 
-	failwith "Implement"
-
-let dTree_is_perfect (t:dTree) : bool = 
-	failwith "Implement"
-
-let dTree_map f g (t:dTree) : dTree = 
-	failwith "Implement"
-
-let list_to_tree (l: char list) : dTree = 
-	failwith "Implement"
-
-let replace_leaf_at (t:dTree) f : dTree = 
+let replace_leaf_at t f = 
 	failwith "Implement"
 
 let bf_to_dTree = 
