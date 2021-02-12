430c70d99f0bba677c10e6ced6bcbdda15f02871
Eric Altenburg <ealtenbu@stevens.edu>
2021-02-11 23:48:09 -0500

    Added more problems from hw1

--- ta_s21/assignments/hw1/hw1.ml
+++ ta_s21/assignments/hw1/hw1.ml
@@ -31,3 +31,95 @@ let rotate_90_letter p =
 
 let rotate_90_word (p: int list list): int list list = 
 	List.map rotate_90_letter p
+
+(* let rec repeat n x = 
+	match n with
+	| 0 -> []
+	| m -> 
+		match m with
+		| 0 -> [0]
+		| 1 -> [1]
+		| _ -> x :: repeat (n-1) x
+
+let rec repeat_p n x = 
+	match n with
+	| 0 -> []
+	| m -> 
+		match m with 
+		| 0 ->[0]
+		| 1 -> [1]
+		| n -> [n] @ repeat (n-1) x
+
+let pantograph_p n l = 
+	List.map (repeat n) l
+
+let rec pantograph n l = 
+	match l with
+	| [] -> []
+	| h::t -> 
+		match h with
+		| 0 -> 0 :: pantograph n t
+		| 1 -> 1 :: pantograph n t
+		| _ -> 3 :: pantograph n t *)
+
+(* let rec repeat n x = 
+	match n with 
+	| 0 -> []
+	| m -> x :: repeat (m-1) x *)
+
+let rec greater_than_one l = 
+	match l with 
+	| [] -> []
+	| h::t -> 
+		if h>1
+		then h::greater_than_one t
+		else greater_than_one t
+
+let rec condense_lists l = 
+	match l with
+	| [] -> []
+	| h::t -> h @ condense_lists t
+
+let rec repeat' n x = 
+	match n with
+	| 0 -> []
+	| m -> 
+		if x > 1
+		then x :: repeat' (m-1) x
+		else x :: repeat' 0 x
+(* 
+let rec repeat_helper n x=
+	match n with
+	| 0 -> []
+	| m -> x :: repeat_helper (n-1) x *)
+(* 
+let rec repeat n x =
+	match x with
+	| 0 -> [0]
+	| 1 -> [1]
+	| m -> repeat_helper n x *)
+
+let rec pantograph n lst = 
+	List.map (repeat' n) lst
+
+let rec count_how_many n prv lst = 
+	match lst with
+	| [] -> n
+	| h::t ->
+		if h==prv
+		then count_how_many (n+1) h t
+		else n
+
+let rec grab_next_few num lst = 
+	match num with
+	| 0 -> lst
+	| m -> grab_next_few (num-1) (List.tl lst)
+
+let rec compress lst =
+	match lst with
+	| [] -> []
+	| h::t -> (h, count_how_many 0 (List.hd lst) lst) :: compress (grab_next_few (count_how_many 0 (List.hd lst) lst) lst)
+
+
+
+
